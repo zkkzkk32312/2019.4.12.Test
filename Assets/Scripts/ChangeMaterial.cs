911d9a66a0ad5a9b7a8e977d0b1a4ced4fb2e6a7
Zack <ZCheng@exergysolutions.com>
2020-10-29 17:05:53 -0600

    initial commit

---
+++ Assets/Scripts/ChangeMaterial.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ChangeMaterial : MonoBehaviour
+{
+    public Material redMatial;
+    public Material blueMaterial;
+    private Renderer renderer;
+    private bool isRed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.B))
+        {
+            renderer.material = blueMaterial;
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            renderer.material = redMatial;
+        }
+    }
+}
