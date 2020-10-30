7cfd75e1be33da3fe5958ca4e98372bbb3c7c543
Sam Goodwin <sgoodwin@gangsterenterprises.com>
2020-10-30 15:02:16 -0600

    Assignment #1 complete sensei

---
+++ Assets/Scripts/Penis.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Penis : MonoBehaviour
+{
+
+    public GameObject penis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(penis, new Vector3(0,100.0f,0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
